bd5d26d2f053476a3213f615fd365d5b7bf0509e
Juan Sebastian Ospina Maya 2041554 <juan.sebastian.ospina@correounivalle.edu.co>
2021-12-06 18:27:00 -0500

    added class Dado

--- src/juegoCraps/Dado.java
+++ src/juegoCraps/Dado.java
@@ -1,4 +1,23 @@
 package juegoCraps;
 
+import java.util.Random;
+
+/**
+ * Class Dado generate a Random value between 1 and 6
+ * @author Juan Sebastian Ospina Maya
+ * @version v1.0.0 date 06/12/2021
+ */
 public class Dado {
+
+    private int cara;
+
+    /**
+     * Method that generate and random value to cara
+     * @return number between (1,6)
+     */
+    public int getCara() {
+        Random aleatorio = new Random();
+        cara = aleatorio.nextInt(6) + 1;
+        return cara;
+    }
 }
